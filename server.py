ce27cc3ca46a3b2b6d7f6d2b2c00a91ac0fd3458
Ivan G <igutierrez0794@gmail.com>
2024-02-28 11:18:23 -0500

    added logic for a 5 guess limit, as well as whether user gets it correct, with a reset button

--- server.py
+++ server.py
@@ -8,17 +8,32 @@ app.secret_key = 'lets guess the number'
 def home():
     if 'random_num' not in session:
         session['random_num'] = random.randint(1, 100)
+    if 'count' not in session:
+        session['count'] = 0
     print(session['random_num'])
     return render_template('index.html')
 
 @app.route('/guess', methods=['POST'])
 def guess():
     if int(request.form['number_guess']) > int(session['random_num']):
-        session['message'] = 'Too high!'
+        session['message'] = 'too high!'
+        session['count'] += 1
+        print(session['count'])
         return redirect('/')
     elif int(request.form['number_guess']) < int(session['random_num']):
-        session['message'] = 'Too low!'
+        session['message'] = 'too low!'
+        session['count'] += 1
+        print(session['count'])
         return redirect('/')
+    else:
+        if int(request.form['number_guess']) == int(session['random_num']):
+            session['message'] = 'correct!'
+            return redirect('/')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
 
 if __name__=='__main__':
     app.run(debug=True)
